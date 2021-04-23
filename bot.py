84d22fac46f829840bc9ae329ab490b3db335a23
BlindStrryke <83036353+BlindStrryke@users.noreply.github.com>
2021-04-23 22:22:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def help(ctx, args=None):
 
     await ctx.send(embed=help_embed)
 
-MOD  
+  
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
@@ -125,7 +125,7 @@ async def avatar(ctx, *,  avamember : discord.Member=None):
     userAvatarUrl = avamember.avatar_url
     await ctx.send(userAvatarUrl)
 
-FUN COMMANDS 
+
 
 #Fun commands
 @bot.command()
@@ -162,7 +162,7 @@ async def _8ball(ctx, *, question):
     responce = random.choice(responses)
     await ctx.send(f'Question: {question}\nAnswer: {responce}')
 
-Greetings
+
 
 @bot.command()
 async def say(ctx, *, question):
